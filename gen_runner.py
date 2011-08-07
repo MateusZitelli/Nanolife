9d2ece9d76a41158cb83416505bb6ae7d645bda1
Mateus Zitelli <zitellimateus@gmail.com>
2011-08-07 14:52:17 -0300

    GPL header added in main.c and gen_runner.py

--- gen_runner.py
+++ gen_runner.py
@@ -1,3 +1,21 @@
+#Nanolife - Simple artificial life simulator
+
+#Copyright 2011 Mateus Zitelli <zitellimateus@gmail.com>
+
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#MA 02110-1301, USA.
 from random import choice
 prog = [int(i) for i in open('creat', 'r').read().split(", ")[:-1]]
 times = [0 for i in range(len(prog))]
